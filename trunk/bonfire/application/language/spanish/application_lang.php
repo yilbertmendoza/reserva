29aa02147bc7610f79f2b0485dd5f9f5bcdad113
darcila <darcila@44131a63-88b7-85e1-e5d1-40e3d3355dca>
2011-06-29 19:49:23 +0000

    se inserta el idioma español

---
+++ trunk/bonfire/application/language/spanish/application_lang.php
@@ -0,0 +1,46 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+//--------------------------------------------------------------------
+// !SETTINGS
+//--------------------------------------------------------------------
+
+$lang['bf_site_name']			= 'Nombre del sitio';
+$lang['bf_site_email']			= 'Correo del sitio';
+$lang['bf_site_email_help']		= 'The default email that system-generated emails are sent from.';
+$lang['bf_site_status']			= 'Estado del sitio';
+$lang['bf_online']				= 'en linea';
+$lang['bf_offline']				= 'desconectado';
+$lang['bf_top_number']			= 'Top How Many?';
+$lang['bf_top_number_help']		= 'When viewing reports, how many items should be listed at a time?';
+
+$lang['bf_security']			= 'Seguridad';
+$lang['bf_login_type']			= 'Tipo de login';
+$lang['bf_login_type_email']	= 'Solo correo';
+$lang['bf_login_type_username']	= 'Solo nombre de usuario';
+$lang['bf_allow_register']		= 'Permitir auto registro de usuarios?';
+$lang['bf_login_type_both']		= 'Correo o nombre de usuario';
+$lang['bf_use_usernames']		= 'Usar nombres de usuario';
+$lang['bf_allow_remember']		= 'Permitir \'Recordarme\'?';
+$lang['bf_remember_time']		= 'Recordar usuarios para';
+$lang['bf_week']				= 'Semana';
+$lang['bf_weeks']				= 'Semanas';
+$lang['bf_days']				= 'Dias';
+
+$lang['bf_home_page']			= 'Página de inicio';
+$lang['bf_pages']				= 'Páginas';
+$lang['bf_enable_rte']			= 'Enable RTE for pages?';
+$lang['bf_rte_type']			= 'RTE Type';
+$lang['bf_searchable_default']	= 'Searchable by default?';
+$lang['bf_cacheable_default']	= 'Cacheable by default?';
+$lang['bf_track_hits']			= 'Track Page Hits?';
+
+$lang['bf_context_content']		= 'Contenido';
+$lang['bf_context_stats']		= 'Estadisticas';
+$lang['bf_context_looks']		= 'Apariencia';
+$lang['bf_context_settings']	= 'Configuración';
+$lang['bf_context_developer']	= 'Desarrollador';
+
+$lang['bf_action_save']			= 'Guardar';
+$lang['bf_action_delete']		= 'Borrar';
+
+$lang['bf_update_show_edge']	= 'View bleeding edge updates?';
